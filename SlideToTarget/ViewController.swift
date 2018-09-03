5904522942ab1d9b758282d9f9761b915afb5810
Fall Thiam <thiam.fall.perso@gmail.com>
2018-09-03 16:53:57 -0400

    backbone of storyboard

--- SlideToTarget/ViewController.swift
+++ SlideToTarget/ViewController.swift
@@ -10,18 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
 
     @IBAction func showAlert(){
-        let alert = UIAlertController(title: "Hello world", message: "This is my first app", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let message = "Current value is \(currentValue)" +
+        " the target value is \(targetValue)"
+        let alert = UIAlertController(title: "Current value is :", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-
+    
+    @IBAction func sliderValue(_ slider: UISlider){
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        targetValue = Int(arc4random_uniform(99)) + 1
+    }
+    
+    func startNewRound(){
+        targetValue = Int(arc4random_uniform(99)) + 1
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 
 }
 
